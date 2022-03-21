109ba34dc710a134cc20da6f0c3feb67ba364bd2
Mason Youngblood <masonyoungblood@gmail.com>
2022-03-21 06:19:47 -0400

    Bayesian GLMM for the cluster

--- analysis/shuffling_model/bayesian_glmm.R
+++ analysis/shuffling_model/bayesian_glmm.R
@@ -1,10 +1,6 @@
 #load packages and data
-setwd("~/Documents/Work/Summer_2021/Cattle_Brands/CattleBrandABM")
-library(brms)
-load("analysis/shuffling_model/shuffling_data.RData")
-
-#data is zero inflated (8.5% of values)
-length(which(shuffling_data$prob_score == 0))/nrow(shuffling_data)
+setwd(system("pwd", intern = T))
+load("shuffling_data.RData")
 
 #drop zeroes from the data
 shuffling_data <- shuffling_data[-which(shuffling_data$prob_score == 0), ]
@@ -16,12 +12,19 @@ shuffling_data$brand <- as.factor(shuffling_data$brand)
 shuffling_data$brand_num <- as.numeric(shuffling_data$brand)
 shuffling_data$mixed <- ifelse(shuffling_data$mixed, 1, 0)
 shuffling_data$complexity <- shuffling_data$complexity-2
+shuffling_data$inv_complexity <- shuffling_data$complexity*-1
 
 #scale and log transform the probability score
 shuffling_data$prob_score <- as.numeric(scale(log(shuffling_data$prob_score)))
 
-#run mixed model
-mixed_model <- REPLACE
-
-#save output
-save(mixed_model, file = "mixed_model.RData")
+#run brms model
+bayesian_glmm <- brms::brm(data = shuffling_data, family = gaussian(),
+                           prob_score ~ inv_complexity + actual*mixed + (1|brand),
+                           prior = c(prior(normal(0, 1), class = Intercept),
+                                     prior(normal(0, 0.5), class = b, coef = "actual"),
+                                     prior(normal(0, 0.5), class = b, coef = "mixed"),
+                                     prior(normal(0, 0.5), class = b, coef = "actual:mixed"),
+                                     prior(lognormal(0, 1), class = b, coef = "inv_complexity"),
+                                     prior(exponential(3), class = sigma)),
+                           cores = 4, iter = 4000, normalize = FALSE, backend = "cmdstanr")
+save(bayesian_glmm, file = "bayesian_glmm.RData")
--- analysis/shuffling_model/glmm.R
+++ analysis/shuffling_model/glmm.R
@@ -3,6 +3,8 @@ setwd("~/Documents/Work/Summer_2021/Cattle_Brands/CattleBrandABM")
 library(performance)
 library(DHARMa)
 library(lme4)
+library(specr)
+library(cowplot)
 load("analysis/shuffling_model/shuffling_data.RData")
 
 #data is zero inflated (8.5% of values)
@@ -22,27 +24,35 @@ shuffling_data$complexity <- shuffling_data$complexity-2
 #scale and log transform the probability score
 shuffling_data$prob_score <- as.numeric(scale(log(shuffling_data$prob_score)))
 
-#run all models
+#run all random intercept models
 space_model <- lmer(prob_score ~ (1|space), data = shuffling_data)
 time_model <- lmer(prob_score ~ (1|time), data = shuffling_data)
 brand_model <- lmer(prob_score ~ (1|brand), data = shuffling_data)
-complexity_model <- lmer(prob_score ~ complexity + (1|brand), data = shuffling_data)
-actual_model <- lmer(prob_score ~ actual + complexity + (1|brand), data = shuffling_data)
-mixed_model <- lmer(prob_score ~ complexity + actual*mixed + (1|brand), data = shuffling_data)
 
 #get intra-class coefficients for each random effect
 performance::icc(space_model)
 performance::icc(time_model)
 performance::icc(brand_model)
 
-#compare complexity model with random effects
-compare_performance(brand_model, complexity_model, metrics = "AIC")
+#add fixed effects and controls to models
+complexity_model <- lmer(prob_score ~ complexity + (1|brand), data = shuffling_data)
+actual_model <- lmer(prob_score ~ actual + complexity + (1|brand), data = shuffling_data)
+mixed_model <- lmer(prob_score ~ complexity + actual*mixed + (1|brand), data = shuffling_data)
 
-#compare complexity and actual model
-compare_performance(complexity_model, actual_model, metrics = "AIC")
+#get WAIC values for competing models
+#brand_waic <- blmeco::WAIC(brand_model)
+#complexity_waic <- blmeco::WAIC(complexity_model)
+#actual_waic <- blmeco::WAIC(actual_model)
+#mixed_waic <- blmeco::WAIC(mixed_model)
+#waic <- list(brand_waic = brand_waic, complexity_waic = complexity_waic, actual_waic = actual_waic, mixed_waic = mixed_waic)
+#save(waic, file = "analysis/shuffling_model/waic.RData")
+load("analysis/shuffling_model/waic.RData")
 
-#compare actual and mixed model
-compare_performance(actual_model, mixed_model, metrics = "AIC")
+#compare WAIC values
+waic$brand_waic$WAIC1
+waic$complexity_waic$WAIC1
+waic$actual_waic$WAIC1
+waic$mixed_waic$WAIC1
 
 #get model estimates from mixed model
 summary(mixed_model)
@@ -56,3 +66,41 @@ qqnorm(residuals(mixed_model))
 #do DHARMa checks (do not look good, so we should re-run a bayesian version of the full model)
 simres <- simulateResiduals(mixed_model)
 plot(simres)
+
+#specification curve analysis: https://masurp.github.io/specr/articles/random_effects.html
+null <- function(formula, data, ...){
+  require(lme4)
+  require(broom.mixed)
+  formula <- paste(formula)
+  lm(formula, data)
+}
+brand <- function(formula, data, ...){
+  require(lme4)
+  require(broom.mixed)
+  formula <- paste(formula, "+ (1|brand)")
+  lmer(formula, data)
+}
+time <- function(formula, data, ...){
+  require(lme4)
+  require(broom.mixed)
+  formula <- paste(formula, "+ (1|time)")
+  lmer(formula, data)
+}
+space <- function(formula, data, ...){
+  require(lme4)
+  require(broom.mixed)
+  formula <- paste(formula, "+ (1|space)")
+  lmer(formula, data)
+}
+
+#run models
+spec_curve_models <- run_specs(df = shuffling_data,
+                               y = c("prob_score"),
+                               x = c("actual", "mixed"),
+                               model = c("null", "brand", "space", "time"),
+                               controls = c("complexity"))
+
+#save plot
+png("analysis/shuffling_model/spec_curve.png", units = "in", width = 5, height = 4.2, res = 300)
+plot_specs(spec_curve_models, choices = c("x", "model", "controls"))
+dev.off()
