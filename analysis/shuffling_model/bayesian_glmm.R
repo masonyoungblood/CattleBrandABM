58f46a8b51e358028f51f475464d0c2a9a51b25f
Mason Youngblood <masonyoungblood@gmail.com>
2022-03-21 12:08:22 +0100

    Loading brms package in Bayesian GLMM script

--- analysis/shuffling_model/bayesian_glmm.R
+++ analysis/shuffling_model/bayesian_glmm.R
@@ -1,6 +1,7 @@
 #load packages and data
 setwd(system("pwd", intern = T))
 load("shuffling_data.RData")
+library(brms)
 
 #drop zeroes from the data
 shuffling_data <- shuffling_data[-which(shuffling_data$prob_score == 0), ]
@@ -18,13 +19,13 @@ shuffling_data$inv_complexity <- shuffling_data$complexity*-1
 shuffling_data$prob_score <- as.numeric(scale(log(shuffling_data$prob_score)))
 
 #run brms model
-bayesian_glmm <- brms::brm(data = shuffling_data, family = gaussian(),
-                           prob_score ~ inv_complexity + actual*mixed + (1|brand),
-                           prior = c(prior(normal(0, 1), class = Intercept),
-                                     prior(normal(0, 0.5), class = b, coef = "actual"),
-                                     prior(normal(0, 0.5), class = b, coef = "mixed"),
-                                     prior(normal(0, 0.5), class = b, coef = "actual:mixed"),
-                                     prior(lognormal(0, 1), class = b, coef = "inv_complexity"),
-                                     prior(exponential(3), class = sigma)),
-                           cores = 4, iter = 4000, normalize = FALSE, backend = "cmdstanr")
+bayesian_glmm <- brm(data = shuffling_data, family = gaussian(),
+                     prob_score ~ inv_complexity + actual*mixed + (1|brand),
+                     prior = c(prior(normal(0, 1), class = Intercept),
+                               prior(normal(0, 0.5), class = b, coef = "actual"),
+                               prior(normal(0, 0.5), class = b, coef = "mixed"),
+                               prior(normal(0, 0.5), class = b, coef = "actual:mixed"),
+                               prior(lognormal(0, 1), class = b, coef = "inv_complexity"),
+                               prior(exponential(3), class = sigma)),
+                     cores = 4, iter = 4000, normalize = FALSE, backend = "cmdstanr")
 save(bayesian_glmm, file = "bayesian_glmm.RData")
